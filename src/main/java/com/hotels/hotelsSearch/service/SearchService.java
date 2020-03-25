24f07d6568d9b46046407c0c02880aea47758679
sshingh67 <singhsharandeep676@gmail.com>
2020-03-24 19:14:43 -0700

    search interface for services

---
+++ src/main/java/com/hotels/hotelsSearch/service/SearchService.java
@@ -0,0 +1,9 @@
+package com.hotels.hotelsSearch.service;
+
+import com.hotels.hotelsSearch.models.Hotel;
+
+import java.util.List;
+
+public interface SearchService {
+    String retrieveHotel(String location);
+}
