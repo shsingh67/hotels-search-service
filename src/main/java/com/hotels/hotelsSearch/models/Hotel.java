5263e7ae30c8cd2ccf2d04b4d8d69bf132d8d3dc
sshingh67 <singhsharandeep676@gmail.com>
2020-03-24 19:14:25 -0700

    model class for hotels

---
+++ src/main/java/com/hotels/hotelsSearch/models/Hotel.java
@@ -0,0 +1,50 @@
+package com.hotels.hotelsSearch.models;
+
+public class Hotel {
+
+    private String id;
+    private String name;
+    private String imageUrl;
+    private int rating;
+    private int reviewCount;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public int getReviewCount() {
+        return reviewCount;
+    }
+
+    public void setReviewCount(int reviewCount) {
+        this.reviewCount = reviewCount;
+    }
+}
